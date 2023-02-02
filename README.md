042df7350d27546f83f2730b13b6ebfa286cc471
Enaium <wyy-666@hotmail.com>
2023-02-02 16:13:17 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Example Cafully Plugin
+
+# License
+
+This template is available under the CC0 license. Feel free to learn from it and incorporate it in your own projects.
---
+++ src/main/java/cn/enaium/cafully/example/Example.java
@@ -0,0 +1,16 @@
+package cn.enaium.cafully.example;
+
+import cn.enaium.cafully.plugin.annotation.Plugin;
+import cn.enaium.cafully.plugin.api.IInitializer;
+import cn.enaium.cafully.plugin.helper.IHelper;
+
+/**
+ * @author Enaium
+ */
+@Plugin(unique = "example", name = "Example Plugin", version = "1.0.0", api = ">=1.0.0")
+public class Example implements IInitializer {
+    @Override
+    public void initialize(IHelper plugin) throws Throwable {
+        plugin.plugin().logger().info("Hello Example World!");
+    }
+}
